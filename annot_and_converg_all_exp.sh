d53e675777cc28f7e18634a408f1f33641b12b03
pphaneuf <pphaneuf@eng.ucsd.edu>
2020-03-13 19:33:08 -0700

    removed obsolete code

--- annot_and_converg_all_exp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sh ./aledb_parse_annot_assoc.sh
-~/anaconda3/bin/python3 converg_all_exp.py
--- exp_converge.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./6_genomic_feature_convergence.ipynb
-~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./7_genetic_convergence.ipynb
-~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./8_operon_convergence.ipynb
-# ~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./9_imodulon_annot.ipynb
-~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./10_COG_convergence.ipynb
-~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./11_regulon_convergence.ipynb
-~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./12_pathway_convergence.ipynb
-~/anaconda3/bin/python archive_exp_converg_data.py
