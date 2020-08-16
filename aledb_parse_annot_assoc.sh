ea46fd7e6d33fd48637dac828f737aead804da22
pphaneuf <pphaneuf@eng.ucsd.edu>
2020-08-15 18:13:57 -0700

    initial commit

---
+++ aledb_parse_annot_assoc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./make_all_AVA_data_dfs.ipynb
+~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./1_aledb_mutation_df.ipynb
+~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./1_5_annot_genetic_feats.ipynb
+~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./2_annot_mut_components.ipynb
+~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./2_2_annot_genomic_feats.ipynb
+~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./2_2_1_get_genetic_links.ipynb
+~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./2_3_annot_operons.ipynb
+~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./2_4_annot_regulons.ipynb
+~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./2_5_annot_pathways.ipynb
+~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./2_6_annot_COGs.ipynb
+~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./2_7_annot_imods.ipynb
+~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./4_assoc_genomic_feat_to_cond.ipynb
+~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./4_1_assoc_imod_to_cond.ipynb
+~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./4_5_assoc_genetic_feat_to_cond.ipynb
+~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./4_6_assoc_regulon_to_cond.ipynb
+~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./4_7_assoc_operon_to_conds.ipynb
+~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./4_8_assoc_pathway_to_conds.ipynb
