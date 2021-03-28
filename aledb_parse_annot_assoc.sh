5513e52e5b070adf9f2d914ca204a6db5b6abfab
pphaneuf <pphaneuf@eng.ucsd.edu>
2021-03-28 13:11:50 -0700

    udpate

--- aledb_parse_annot_assoc.sh
+++ aledb_parse_annot_assoc.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./make_all_AVA_data_dfs.ipynb
+'''~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./make_all_AVA_data_dfs.ipynb
 ~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./1_aledb_mutation_df.ipynb
 ~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./1_5_annot_genetic_feats.ipynb
 ~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./2_annot_mut_components.ipynb
 ~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./2_2_annot_genomic_feats.ipynb
 ~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./2_2_1_get_genetic_links.ipynb
-~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./2_3_annot_operons.ipynb
+~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./2_3_annot_operons.ipynb'''
 ~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./2_4_annot_regulons.ipynb
 ~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./2_5_annot_pathways.ipynb
 ~/anaconda3/bin/jupyter nbconvert --execute --to notebook --ExecutePreprocessor.timeout=-1 --inplace ./2_6_annot_COGs.ipynb
